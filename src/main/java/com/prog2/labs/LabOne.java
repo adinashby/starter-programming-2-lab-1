82ef1bdcb2835efe2cf1ce16568816edd4ab2b1b
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2023-02-06 12:10:26 -0500

    Update LabOne.java

--- src/main/java/com/prog2/labs/LabOne.java
+++ src/main/java/com/prog2/labs/LabOne.java
@@ -15,54 +15,20 @@ public class LabOne {
 	}
 	
 	/**
-	 * 1) Write a Java method to concatenate a given string to the end of another string.
-	 * 
-	 * @param firstString first input string.
-	 * @param secondString second input string.
-	 *
-	 * @return result concatenated string.
+	 * Please refer to the README file for question(s) description
 	 */
 	
 	public String concatStringToEnd(String firstString, String secondString) {
 		return "";
 	}
 	
-	/**
-	 * 2) Write a Java method to check whether a given string ends with 
-	 * the contents of another string. 
-	 * You can ask the user for the input strings.
-	 * 
-	 * @param firstString first input string.
-	 * @param secondString second input string.
-	 *
-	 * @return result true if the first string is ending with the second string.
-	 */
-	
 	public boolean doesEndWithSecondString(String firstString, String secondString) {
 		return false;
 	}
 	
-	/**
-	 * 3) Write a Java method to generate random integers in a specific range.
-	 * 
-	 * @param startingNumber starting number.
-	 * @param endingNumber ending number.
-	 *
-	 * @return result randomly generated number within a range.
-	 */
-	
 	public int randomIntegerGenerator(int startingNumber, int endingNumber) {
 		return 0;
 	}
-	
-	/**
-	 * 4) Write a Java method to check whether a password is valid or not. 
-	 * A valid password should contain an uppercase letter, lowercase letter, and digit.
-	 * 
-	 * @param password input password.
-	 *
-	 * @return result true if the password is valid.
-	 */
 	
 	public boolean isPasswordValid(String password) {
 		return false;
